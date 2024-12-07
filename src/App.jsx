d4c1d2f7ecc9c059fc0f6e2e8d0841f31db5d4ba
Song da young <116068642+butcreep@users.noreply.github.com>
2024-12-08 00:26:57 +0900

    feat: 프로젝트 초기 설정 및 기본 구조 구성
    - React Router 설정 및 라우트 구성
    - Home 페이지 기본 컴포넌트 생성
    - 스타일링 방식 논의 및 Tailwind CSS 설치 준비
    - Node.js 버전 문제 해결 및 종속성 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import styled from "styled-components";
+// import LetterWrite from "./pages/LetterWrite";
+const AppContainer = styled.div`
+  margin: 0 auto;
+  max-width: 480px;
+  overflow: auto;
+  width: 100%;
+  background-color: #fff;
+  height: calc(var(--vh, 1vh) * 100);
+`;
 
 const App = () => {
   return (
-    <div>
-      <h1>four letters</h1>
-    </div>
+    <AppContainer>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/write" element={<LetterWrite />} /> */}
+      </Routes>
+    </AppContainer>
   );
 };
 
