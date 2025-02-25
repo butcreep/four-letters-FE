4a0cd876bb078bf0acca5735e1c9b49321fc4df7
Song da young <116068642+butcreep@users.noreply.github.com>
2025-02-25 21:18:50 +0900

    refactor: api 주소 변경

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import store from "../redux/store";
 
 const apiClient = axios.create({
-  // baseURL: "https://mushy-marisa-dgnppr-a63d5cc2.koyeb.app/api/v1",
-  baseURL: process.env.REACT_APP_GLITCH_URL,
+  baseURL: "https://mushy-marisa-dgnppr-a63d5cc2.koyeb.app/api/v1",
+  // baseURL: process.env.REACT_APP_GLITCH_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-apiClient.interceptors.request.use(config => {
+apiClient.interceptors.request.use((config) => {
   const state = store.getState();
   const token = state.user?.token;
 
